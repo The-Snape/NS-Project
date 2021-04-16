1bcd2dae8f1fa95f11c3d7305116b93a910fe3af
BB <bbsharma22111998@gmail.com>
2021-04-16 23:41:45 +0530

    Baisc

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (  
+        <div>
+            Hola Amigos!!
+        </div>
+    );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+        <h1>Network Security</h1>
+        <div class="navbar">
+            <Link to="/">Home</Link>
+            <div class="dropdown">
+                <button class="dropbtn">Assignment 
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <Link to="/pa1">Practical Assignment 1</Link>
+                </div>
+            </div> 
+        </div>
+      </nav>
+    );
+}
+ 
+export default Navbar;
